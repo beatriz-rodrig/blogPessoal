1f7ae0924647ff507aeba1bf39c40bc9267d01e8
beatriz-rodrig <beatriz_rodrigues1993@hotmail.com>
2021-09-27 22:05:41 -0300

    aulas

--- src/main/java/org/generation/blogPessoal/repository/PostagemRepository.java
+++ src/main/java/org/generation/blogPessoal/repository/PostagemRepository.java
@@ -4,11 +4,15 @@ import java.util.List;
 
 import org.generation.blogPessoal.model.Postagem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
 
+	
 	public List <Postagem> findAllByTituloContainingIgnoreCase (String titulo);
+
 	
 }
