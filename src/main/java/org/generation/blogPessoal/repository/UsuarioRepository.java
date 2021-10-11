75f67484f1d9e07c36779f4037fd6ee1da28af16
beatriz-rodrig <beatriz_rodrigues1993@hotmail.com>
2021-10-11 13:13:38 -0300

    fechando o junit

--- src/main/java/org/generation/blogPessoal/repository/UsuarioRepository.java
+++ src/main/java/org/generation/blogPessoal/repository/UsuarioRepository.java
@@ -1,6 +1,7 @@
 package org.generation.blogPessoal.repository;
 
 
+import java.util.List;
 import java.util.Optional;
 
 import org.generation.blogPessoal.model.Usuario;
@@ -10,6 +11,10 @@ public interface UsuarioRepository extends JpaRepository <Usuario, Long> {
 
 	
 	public Optional<Usuario> findByUsuario (String usuario);
+	
+	public List <Usuario> findAllByNomeContainingIgnoreCase (String nome);
+	
+	public Usuario findByNome (String nome);
 
 	
 }
